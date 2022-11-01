e23c9f823956651878bed257a96c522e9b9ba099
SergereAKa <sergeyaka64@gmail.com>
2022-11-01 10:32:27 +0500

    Начальная версия проекта

---
+++ src/main/java/com/ksa/telegram/orangecomplexbot/OrangeComplexBotApplication.java
@@ -0,0 +1,13 @@
+package com.ksa.telegram.orangecomplexbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeComplexBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeComplexBotApplication.class, args);
+	}
+
+}
